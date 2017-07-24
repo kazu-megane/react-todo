ee699feb0f747c4668f82c706b1a7f4b3b4c0cc2
kazu-megane <kazukazu.music1214@gmail.com>
2017-07-24 15:27:56 +0900

    Add update_method

--- src/Todo.js
+++ src/Todo.js
@@ -3,13 +3,13 @@ import React, {Component} from 'react';
 
 class Todo extends Component{
     render(){
-        const className = 'undone';
+        const className = this.props.done ? 'done' : 'undone';
         const link = this.props.done ? '元に戻す' : '完了！';
         return(
             <li className={className}>
                 <span>{this.props.id}</span>
                 <span>:{this.props.title}</span>
-                <a href="">{link}</a>
+                <a href="" onclick={(e) => {e.preventDefault(); this.props.setTodoStatus(this.props)}}>{link}</a>
                 <p>{this.props.desc}</p>
             </li>
         );
